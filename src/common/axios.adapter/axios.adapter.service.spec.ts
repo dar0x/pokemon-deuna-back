bcf595a291e7a69ec8f35ff512be7365da042447
dar0x <axel.darox.ec@gmail.com>
2024-02-25 16:18:27 -0500

    test: unit test for controller and axios adapter are added and tests are covered 100%

--- src/common/axios.adapter/axios.adapter.service.spec.ts
+++ src/common/axios.adapter/axios.adapter.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AxiosAdapterService } from './axios.adapter.service';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('AxiosAdapterService', () => {
   let service: AxiosAdapterService;
@@ -14,5 +18,18 @@ describe('AxiosAdapterService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return data from the API call', (done) => {
+    const mockData = { data: 'test data' };
+    mockedAxios.get.mockResolvedValue(mockData);
+
+    service.get('http://pokeurl.com').subscribe({
+      next: (data) => {
+        expect(data).toEqual('test data');
+        expect(mockedAxios.get).toHaveBeenCalledWith('http://pokeurl.com');
+        done();
+      },
+    });
   });
 });
--- src/pokemon/pokemon.controller.spec.ts
+++ src/pokemon/pokemon.controller.spec.ts
@@ -1,20 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonController } from './pokemon.controller';
 import { PokemonService } from './pokemon.service';
+import { PokemonListResponse } from './interface/pokemon-list-response.interface';
+import { PokemonDetailResponse } from './interface/pokemon-detail-response.interface';
 
 describe('PokemonController', () => {
   let controller: PokemonController;
+  let service: PokemonService;
+  const findAllResponse: PokemonListResponse = {
+    next: '',
+    previous: undefined,
+    count: 1,
+    results: [
+      {
+        name: 'bulbasaur',
+        url: 'some_url',
+      },
+    ],
+  };
+  const detailResponse: PokemonDetailResponse = {
+    base_experience: 0,
+    height: 0,
+    id: 0,
+    is_default: false,
+    location_area_encounters: '',
+    weight: 0,
+    name: 'Pikachu',
+    order: 1,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PokemonController],
-      providers: [PokemonService],
+      providers: [
+        {
+          provide: PokemonService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(findAllResponse),
+            findOne: jest.fn().mockResolvedValue(detailResponse),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<PokemonController>(PokemonController);
+    service = module.get<PokemonService>(PokemonService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return an array of pokemons', async () => {
+    await expect(controller.findAll({ limit: 10, offset: 0 })).resolves.toEqual(
+      findAllResponse,
+    );
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should return a single pokemon', async () => {
+    await expect(controller.findOne('1')).resolves.toEqual(detailResponse);
+    expect(service.findOne).toHaveBeenCalledWith(1);
   });
 });
